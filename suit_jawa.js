37e5639969adadaf79544508757a15e79af7c1ea
Nexvaulus <87605397+Nexvaulus@users.noreply.github.com>
2021-10-02 17:47:09 +0700

    Memperbaiki

--- suit_jawa.js
+++ suit_jawa.js
@@ -1,5 +1,5 @@
 var tanya = true;
-while(tanya) {
+while(tanya == true) {
 
     // menangkap pilihan player
     var player = prompt("Pilih : Gajah, Semut, atau Orang");
@@ -7,12 +7,18 @@ while(tanya) {
 
     // menangkap pilihan computer
     // membangkitkan bilangan random
-    var comp = Math.random();
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+      }
+    
+    var comp = getRandomInt(1, 10);
 
-    if(comp < 0,34) {
+    if(comp < 2) {
         comp = "gajah";
     } 
-    else if(comp >= 0,34 && comp <= 0,67) {
+    else if(comp >= 2 && comp < 7) {
         comp = "semut";
     } 
     else {
@@ -26,41 +32,42 @@ while(tanya) {
     }
 
     else if(player == "gajah" || player == "Gajah") {
-        // if(comp == "orang") {
-        //     hasil = "MENANG"
-        // }
-        // else {
-        //     hasil = "KALAH"
-        // }
-        hasil = (comp == "orang") ? "MENANG" : "KALAH"; // ini adalah ringkasan dari if diatas  
+        if(comp == "orang") {
+            hasil = "MENANG"
+        }
+
+        else if(comp == "semut") {
+            hasil = "KALAH"
+        }
+        // hasil = (comp == "orang") ? "MENANG" : "KALAH"; // ini adalah ringkasan dari if diatas  
     }
 
     else if(player == "semut" || player == "Semut") {
-        // if(comp == "orang") {
-        //     hasil = "KALAH"
-        // }
-        // else {
-        //     hasil = "MENANG"
-        // }
-        hasil = (comp == "orang") ? "KALAH" : "MENANG"; // ini adalah ringkasan dari if diatas  
+        if(comp == "orang") {
+            hasil = "KALAH"
+        }
+        else if(comp == "gajah") {
+            hasil = "MENANG"
+        }
+        // hasil = (comp == "orang") ? "KALAH" : "MENANG"; // ini adalah ringkasan dari if diatas  
     }
 
     else if(player == "orang" || player == "Orang") {
-        // if(comp == "gajah") {
-        //     hasil == "KALAH"
-        // }
-        // else {
-        //     hasil = 'MENANG'
-        // }
-        hasil = (comp == gajah) ? "KALAH" : "Menang"; // ini adalah ringkasan dari if diatas  
+        if(comp == "gajah") {
+            hasil == "KALAH"
+        }
+        else if(player == "semut") {
+            hasil = 'MENANG'
+        }
+        // hasil = (comp == gajah) ? "KALAH" : "Menang"; // ini adalah ringkasan dari if diatas  
     }
     else {
         hasil = "INVALID"
     }
     // tampilkan hasilnya
-    alert("Kamu memilih " + player + " dan komputer memilih " + comp + "\nMakan hasilnya : Kamu " + hasil);
-
+    alert("Kamu memilih " + player + " dan komputer memilih " + comp + "\nMaka hasilnya : Kamu " + hasil);
     tanya = confirm("Lagi?");
+
 }
 
 alert("Terimakasih sudah bermain")
